20d6d1b881400c38675c44b47f62ecef576622be
Nesquik <95046265+NeyTwix@users.noreply.github.com>
2024-10-19 16:26:19 +0200

    Ajout fonction submit et passage sous php

--- src/connexion.php
+++
@@ -1,4 +0,0 @@
-<?php
- echo "ok";
- system("calc");
-?>
--- src/login.php
+++ src/login.php
@@ -10,14 +10,14 @@
 		<div class="login-container">
 			<!-- Espace pour l'image de profil -->
 			<div class="profile-pic">
-				<img src="/src/cdn/photo-profil2.png" alt="Profil" id="profile-img" />
+				<img src="/src/content/photo-profil2.png" alt="Profil" id="profile-img" />
 			</div>
-			<form>
+			<form id="login-form">
 				<!-- Champ pour entrer le nom d'utilisateur -->
-				<input type="text" placeholder="Nom du PC" /><br />
+				<input type="text" id="username" placeholder="Nom du PC" /><br />
 				<!-- Champ pour entrer le mot de passe -->
 				<input type="password" id="pass" placeholder="Mot de passe session" /><br />
-				<img src="/src/cdn/oeil-barree.png" id="eye" alt="Image oeil barré" onClick="changer()" />
+				<img src="/src/content/oeil-barree.png" id="eye" alt="Image oeil barré" onClick="changer()" />
 				<!-- Lien pour récupérer le mot de passe -->
 				<button type="submit" class="login-button">Connexion</button>
 
@@ -30,6 +30,13 @@
 							motDePasse.type = "password";
 						}
 					}
+					document.getElementById("login-form").addEventListener("submit", function(event) {
+                    	event.preventDefault(); // Empêche la soumission du formulaire
+                    	var username = document.getElementById("username").value;
+                    	var password = document.getElementById("pass").value;
+                    	console.info("Nom du PC:", username);
+						console.info("Mot de passe session:", password);
+                	});
 				</script>
 			</form>
 		</div>
