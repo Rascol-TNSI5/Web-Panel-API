8ffc4c43be32af38f56b835df57c687044005b0e
endelf <93210351+endelf@users.noreply.github.com>
2024-11-15 07:09:44 +0100

    webhook / client

--- src/webhook/client.php
+++ src/webhook/client.php
@@ -8,15 +8,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode($json, true);
     
     // Vérifie si le champ client_id existe dans les données
-    if (isset($data['client_id'])) {
+    if (isset($data['uid']) && isset($data['computer_name'])) {
         // Récupère le client_id
-        $client_id = $data['client_id'];
-
+        $client_uid = $data['uid'];
+        $computer_name = $data['computer_name']
+        $username = $data['username']
+        $architecture = $data['architecture']
+        $windows_version = $data['windows_version']
+        
         // Prépare le contenu à écrire dans le fichier
-        $log_entry = "client_id: " . $client_id . "\n";
+        $log_entry = "uid: " . $client_id . "," . " computer_name:" . $computer_name ;
 
         // Chemin vers le fichier texte où nous allons enregistrer les données
-        $file_path = 'client_ids.txt';
+        $file_path = 'clients.txt';
 
         // Écrit le contenu dans le fichier
         file_put_contents($file_path, $log_entry, FILE_APPEND | LOCK_EX);
